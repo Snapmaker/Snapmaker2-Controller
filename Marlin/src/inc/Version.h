51af46258c5964f1f6fb47f59682b45ac6ed1d80
Scott Huang <scott.sl.huang@hotmail.com>
2020-03-22 14:21:58 +0800

    NEW: update verion to V1.7.0.0 for release
    Signed-off-by: Scott Huang <scott.sl.huang@hotmail.com>

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -38,7 +38,7 @@
   /**
    * Marlin release version identifier
    */
-  #define SHORT_BUILD_VERSION "SM2-1.6.2.15"
+  #define SHORT_BUILD_VERSION "SM2-1.7.0.0"
 
   /**
    * Verbose version identifier which should contain a reference to the location
