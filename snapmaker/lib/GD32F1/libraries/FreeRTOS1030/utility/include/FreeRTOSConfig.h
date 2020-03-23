d0c24be84c45cf135b83be47c9d7ff1fa6c1ed26
Scott Huang <shili.huang@snapmaker.com>
2020-03-23 16:50:06 +0800

    NEW: change the config per orignal maple freeRTOSConfig.h
    Signed-off-by: Scott Huang <shili.huang@snapmaker.com>

--- snapmaker/lib/GD32F1/libraries/FreeRTOS1030/utility/include/FreeRTOSConfig.h
+++ snapmaker/lib/GD32F1/libraries/FreeRTOS1030/utility/include/FreeRTOSConfig.h
@@ -51,10 +51,10 @@
 #define configUSE_IDLE_HOOK				0
 #define configUSE_TICK_HOOK				0
 #define configCPU_CLOCK_HZ				( F_CPU )
-#define configTICK_RATE_HZ				( ( TickType_t ) 100 )
+#define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES			( 5 )
-#define configMINIMAL_STACK_SIZE		( ( unsigned short ) 130 )
-#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 75 * 1024 ) )
+#define configMINIMAL_STACK_SIZE		( ( unsigned short ) 120 )
+#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 24 * 1024 ) )
 #define configMAX_TASK_NAME_LEN			( 10 )
 #define configUSE_TRACE_FACILITY		0
 #define configUSE_16_BIT_TICKS			0
