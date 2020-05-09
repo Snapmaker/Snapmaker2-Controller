9466f2e22d0c1ce13bb642683c5de02b757b4968
Scott Huang <shili.huang@snapmaker.com>
2020-05-09 09:47:03 +0800

    FIX: increase the stack for heart beat task to avoid overflow
    Signed-off-by: Scott Huang <shili.huang@snapmaker.com>

--- Marlin/src/Marlin.h
+++ Marlin/src/Marlin.h
@@ -456,7 +456,7 @@ extern SnapTasks_t snap_tasks;
 
 // task parameters for heartbeat task
 #define HB_TASK_PRIO             (configMAX_PRIORITIES - 1)
-#define HB_TASK_STACK_DEPTH      configMINIMAL_STACK_SIZE
+#define HB_TASK_STACK_DEPTH      512
 
 #define HMI_SERIAL_IRQ_PRIORITY     8
 #define MARLIN_SERIAL_IRQ_PRIORITY  9
