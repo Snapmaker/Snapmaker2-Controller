445cb50ff2cc6c8108b9fac0c4b52924c5c5bd76
Scott Huang <shili.huang@snapmaker.com>
2020-05-19 15:07:17 +0800

    NEW: update version to V3.0.1.1
    Signed-off-by: Scott Huang <shili.huang@snapmaker.com>

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -38,7 +38,7 @@
   /**
    * Marlin release version identifier
    */
-  #define SHORT_BUILD_VERSION "SM2-3.0.0.5"
+  #define SHORT_BUILD_VERSION "SM2-3.0.1.1"
 
   /**
    * Verbose version identifier which should contain a reference to the location
