e939c23ecb997ed945b5e35ca4641ba63ffe89fb
Scott Huang <shili.huang@snapmaker.com>
2020-10-29 17:41:25 +0800

    Fix: fix bug that may trigger heating failed for hotend
        In V3.2.2, if user start heating hotend from last heating
        in a short time, thermal protect may report heating failed.
        Because the parameters for this protection is too sensitive.
    
    Signed-off-by: Scott Huang <shili.huang@snapmaker.com>

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -94,8 +94,8 @@
    * and/or decrease WATCH_TEMP_INCREASE. WATCH_TEMP_INCREASE should not be set
    * below 2.
    */
-  #define WATCH_TEMP_PERIOD   10                // Seconds
-  #define WATCH_TEMP_INCREASE 1               // Degrees Celsius
+  #define WATCH_TEMP_PERIOD   20                // Seconds
+  #define WATCH_TEMP_INCREASE 2               // Degrees Celsius
 
   #define WATCH_TEMP_TARGET_START   100
 
