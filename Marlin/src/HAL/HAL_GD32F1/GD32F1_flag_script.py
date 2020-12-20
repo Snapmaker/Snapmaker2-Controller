db3a0256f6ef9afaf6890a103f9769d62a50c34a
Marcel Fabian Krüger <snapmaker@2krueger.de>
2020-12-20 22:26:21 +0100

    Perf: Switch to float-abi "hard"

--- Marlin/src/HAL/HAL_GD32F1/GD32F1_flag_script.py
+++ Marlin/src/HAL/HAL_GD32F1/GD32F1_flag_script.py
@@ -1,8 +1,9 @@
 # extra script for linker and compile options
 Import("env")
 env.Append(
+  LINKFLAGS = [ "-mfpu=fpv4-sp-d16", "-mfloat-abi=hard" ], # -mfpu is required here since GCC is outdated
   CCFLAGS=[ "-mfpu=fpv4-sp-d16",
-            "-mfloat-abi=softfp",
+            "-mfloat-abi=hard",
 
             "-fsigned-char",
             "-fno-move-loop-invariants",
