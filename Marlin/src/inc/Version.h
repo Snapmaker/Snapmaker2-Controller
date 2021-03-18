ac383b607abbcb0ea2a269a2a7639b46bc609ca9
xhycode <731374786@qq.com>
2021-03-18 10:53:53 +0800

    Feature: update versions to V4.2.1

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -38,7 +38,7 @@
   /**
    * Marlin release version identifier
    */
-  #define SHORT_BUILD_VERSION "SM2-4.1.0"
+  #define SHORT_BUILD_VERSION "SM2-4.2.1"
 
   /**
    * Verbose version identifier which should contain a reference to the location
@@ -51,7 +51,7 @@
    * here we define this default string as the date where the latest release
    * version was tagged.
    */
-  #define STRING_DISTRIBUTION_DATE "2021-01-28"
+  #define STRING_DISTRIBUTION_DATE "2021-03-16"
 
   /**
    * Required minimum Configuration.h and Configuration_adv.h file versions.
