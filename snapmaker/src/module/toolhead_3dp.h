feaa3c7e3f5e20492adbcbb9569b861ed7e91965
xhycode <731374786@qq.com>
2021-08-24 10:22:46 +0800

    Feature: fix type of 3dp nozzle temperature

--- snapmaker/src/module/toolhead_3dp.h
+++ snapmaker/src/module/toolhead_3dp.h
@@ -95,13 +95,13 @@ class ToolHead3DP: public ModuleBase {
       return filament_state_;
     }
 
-    void SetTemp(uint16_t temp, uint8_t extrude_index=0) {
+    void SetTemp(int16_t temp, uint8_t extrude_index=0) {
       if (extrude_index >= EXTRUDERS)
         return;
 
       cur_temp_[extrude_index] = temp;
     }
-    uint16_t GetTemp(uint8_t extrude_index=0) {
+    int16_t GetTemp(uint8_t extrude_index=0) {
       if (extrude_index >= EXTRUDERS)
         return 0;
 
@@ -116,7 +116,7 @@ class ToolHead3DP: public ModuleBase {
 
     uint16_t timer_in_process_;
 
-    uint16_t cur_temp_[EXTRUDERS];
+    int16_t cur_temp_[EXTRUDERS];
     uint8_t  fan_speed_[TOOLHEAD_3DP_FAN_MAX];
 
     // 1 bit indicates one sensor
