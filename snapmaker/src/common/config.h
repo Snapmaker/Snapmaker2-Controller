6f471de3a822cc87667722dd593c8a37760d1007
xhycode <731374786@qq.com>
2021-09-28 14:44:00 +0800

    Feature: Optimize the main loop delay
    Each delay will reduce the processing efficiency of Gcode,It also increases the laser turn-on time

--- snapmaker/src/common/config.h
+++ snapmaker/src/common/config.h
@@ -22,11 +22,11 @@
 #define SNAPMAKER_CONFIG_H_
 
 // task parameters for can event handler task
-#define CAN_EVENT_HANDLER_PRIORITY    (2)
+#define CAN_EVENT_HANDLER_PRIORITY    (3)
 #define CAN_EVENT_HANDLER_STACK_DEPTH (512)
 
 // task parameters for can receive handler task
-#define CAN_RECEIVE_HANDLER_PRIORITY    (2)
+#define CAN_RECEIVE_HANDLER_PRIORITY    (3)
 #define CAN_RECEIVE_HANDLER_STACK_DEPTH (512)
 
 // task parameters for marlin loop task
@@ -38,7 +38,7 @@
 #define HMI_TASK_STACK_DEPTH 512
 
 // task parameters for heartbeat task
-#define HB_TASK_PRIO (2)
+#define HB_TASK_PRIO (3)
 #define HB_TASK_STACK_DEPTH 512
 
 // priority for UARTs
