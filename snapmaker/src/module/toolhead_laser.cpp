b930ad1bff102bb6cdbb053611b3170aff3bf90f
xhycode <731374786@qq.com>
2021-11-29 16:43:05 +0800

    Fix: 10w laser security power status restore failure

--- snapmaker/src/module/toolhead_laser.cpp
+++ snapmaker/src/module/toolhead_laser.cpp
@@ -75,11 +75,11 @@ static void CallbackAckReportSecurity(CanStdDataFrame_t &cmd) {
 
   if (laser->security_status_ != 0) {
     laser->need_to_turnoff_laser_ = true;
-    if (laser->laser_10w_status_ == LASER_10W_ENABLE) {
-      laser->TurnOff();
-    }
+    
     if (systemservice.GetCurrentStage() == SYSTAGE_WORK || systemservice.GetCurrentStage() == SYSTAGE_PAUSE) {
       quickstop.Trigger(QS_SOURCE_SECURITY, true);
+    } else if (laser->laser_10w_status_ == LASER_10W_ENABLE) {
+      laser->TurnOff();
     }
   }
 }
