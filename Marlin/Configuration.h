c1bfd97a4850d58d80fa1c8fb51ca3b4f128a303
lixiaolong <786503628@qq.com>
2022-07-21 16:16:35 +0800

    Feature: adjust home offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1055,7 +1055,7 @@
 
 #define S_HOME_OFFSET_DEFAULT {-7, -5, 0, 0}
 #define M_HOME_OFFSET_DEFAULT {-15.5, -7, 0, 0}
-#define L_HOME_OFFSET_DEFAULT {-19, -10, 0, 0}
+#define L_HOME_OFFSET_DEFAULT {-19, -4, 0, 0}
 #endif //DISABLE(SW_MACHINE_SIZE)
 
 // The size of the print bed
