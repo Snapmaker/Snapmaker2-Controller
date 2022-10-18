89cd6a8c5d99a019b714b0a235b7aab667e98d55
Scott Huang <shili.huang@snapmaker.com>
2022-10-18 18:43:34 +0800

    Feature: update version to v4.5.7
    Signed-off-by: Scott Huang <shili.huang@snapmaker.com>

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -38,7 +38,7 @@
   /**
    * Marlin release version identifier
    */
-  #define SHORT_BUILD_VERSION "SM2-4.5.6"
+  #define SHORT_BUILD_VERSION "SM2-4.5.7"
 
   /**
    * Verbose version identifier which should contain a reference to the location
