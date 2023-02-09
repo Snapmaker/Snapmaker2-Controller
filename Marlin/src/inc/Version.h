d488b687439e5eaba653ec16d6a3163c737d8bd6
zhangquanyi <zhangquanyi@snapmaker.com>
2023-02-09 10:20:13 +0800

    Feature: update version to v4.5.25

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -38,7 +38,7 @@
   /**
    * Marlin release version identifier
    */
-  #define SHORT_BUILD_VERSION "SM2-4.5.24"
+  #define SHORT_BUILD_VERSION "SM2-4.5.25"
 
   /**
    * Verbose version identifier which should contain a reference to the location
@@ -51,7 +51,7 @@
    * here we define this default string as the date where the latest release
    * version was tagged.
    */
-  #define STRING_DISTRIBUTION_DATE "2023-01-11"
+  #define STRING_DISTRIBUTION_DATE "2023-02-09"
 
   /**
    * Required minimum Configuration.h and Configuration_adv.h file versions.
