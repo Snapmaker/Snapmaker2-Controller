9e3330df66524692b388e4047bc40590ed342353
zhangquanyi <zhangquanyi@snapmaker.com>
2023-08-11 12:23:02 +0800

    Feature: modify the size of BLOCK_BUFFER_SIZE to 16

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1179,7 +1179,7 @@
 #if ENABLED(SDSUPPORT)
   #define BLOCK_BUFFER_SIZE 16 // SD,LCD,Buttons take more memory, block buffer needs to be smaller
 #else
-  #define BLOCK_BUFFER_SIZE 64 // maximize block buffer
+  #define BLOCK_BUFFER_SIZE 16 // maximize block buffer
 #endif
 
 // @section serial
