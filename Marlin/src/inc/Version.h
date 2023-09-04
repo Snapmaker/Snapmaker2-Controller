1da5ef5701f714c8f6b6bbaffa552cc2854b6c63
zhangquanyi <zhangquanyi@snapmaker.com>
2023-09-04 15:49:08 +0800

    Feature: update version to v4.6.1

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -38,7 +38,7 @@
   /**
    * Marlin release version identifier
    */
-  #define SHORT_BUILD_VERSION "SM2-4.6.0"
+  #define SHORT_BUILD_VERSION "SM2-4.6.1"
 
   /**
    * Verbose version identifier which should contain a reference to the location
