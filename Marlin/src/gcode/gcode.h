0f8f655d15194b11eef3f29b4f76e144215452b2
zhangquanyi <zhangquanyi@snapmaker.com>
2023-10-12 17:42:46 +0800

    Feature: setting the safe z-axis position offset when resetting the configuration

--- Marlin/src/gcode/gcode.h
+++ Marlin/src/gcode/gcode.h
@@ -289,6 +289,7 @@ public:
 
   #define MAX_COORDINATE_SYSTEMS 2
   #if ENABLED(CNC_COORDINATE_SYSTEMS)
+    #define SAFE_Z_AXIS_POSITION_SHIFT       -100
     static int8_t active_coordinate_system;
     static float coordinate_system[MAX_COORDINATE_SYSTEMS][XN];
     static bool select_coordinate_system(const int8_t _new);
