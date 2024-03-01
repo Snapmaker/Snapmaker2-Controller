fa096f0eb8841e99e7bb5f4bf6308bc0f73a31c2
Scott Huang <shili.huang@snapmaker.com>
2024-03-01 12:07:44 +0800

    Improvement: increase FT motion traj buffer
    Signed-off-by: Scott Huang <shili.huang@snapmaker.com>

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2374,7 +2374,7 @@
    */
   #define FTM_UNIFIED_BWS                         // DON'T DISABLE unless you use Ulendo FBS (not implemented)
   #if ENABLED(FTM_UNIFIED_BWS)
-    #define FTM_BW_SIZE               100         // Unified Window and Batch size with a ratio of 2
+    #define FTM_BW_SIZE               200         // Unified Window and Batch size with a ratio of 2
   #else
     #define FTM_WINDOW_SIZE           200         // Custom Window size for trajectory generation needed by Ulendo FBS
     #define FTM_BATCH_SIZE            100         // Custom Batch size for trajectory generation needed by Ulendo FBS
