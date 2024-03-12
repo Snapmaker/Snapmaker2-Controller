5a6379889c4221b172418be03735a450f2620e23
Scott Huang <shili.huang@snapmaker.com>
2024-03-12 11:44:53 +0800

    Improvement: reduce freeRTOS heap to save memory
    Signed-off-by: Scott Huang <shili.huang@snapmaker.com>

--- snapmaker/lib/GD32F1/libraries/FreeRTOS1030/utility/include/FreeRTOSConfig.h
+++ snapmaker/lib/GD32F1/libraries/FreeRTOS1030/utility/include/FreeRTOSConfig.h
@@ -54,7 +54,7 @@
 #define configTICK_RATE_HZ				( ( TickType_t ) 1000 )
 #define configMAX_PRIORITIES			( 4 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 120 )
-#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 36 * 1024 ) )
+#define configTOTAL_HEAP_SIZE			( ( size_t ) ( 30 * 1024 ) )
 #define configMAX_TASK_NAME_LEN			( 10 )
 #define configUSE_TRACE_FACILITY		1
 #define configUSE_16_BIT_TICKS			0
@@ -113,11 +113,11 @@ to all Cortex-M ports, and do not rely on any particular library functions. */
 /* !!!! configMAX_SYSCALL_INTERRUPT_PRIORITY must not be set to zero !!!!
 See http://www.FreeRTOS.org/RTOS-Cortex-M3-M4.html. */
 #define configMAX_SYSCALL_INTERRUPT_PRIORITY 	( configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY << (8 - configPRIO_BITS) )
-	
+
 /* Normal assert() semantics without relying on the provision of an assert.h
 header file. */
-#define configASSERT( x ) if( ( x ) == 0 ) { taskDISABLE_INTERRUPTS(); for( ;; ); }	
-	
+#define configASSERT( x ) if( ( x ) == 0 ) { taskDISABLE_INTERRUPTS(); for( ;; ); }
+
 /* Definitions that map the FreeRTOS port interrupt handlers to their CMSIS
 standard names. */
 #define vPortSVCHandler SVC_Handler
