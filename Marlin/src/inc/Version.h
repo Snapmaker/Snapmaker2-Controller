9888958c75251c9435b800439ff4e83ed8882ebc
Scott Huang <shili.huang@snapmaker.com>
2024-04-07 15:05:55 +0800

    Feature: update version to v5.1.1
    Signed-off-by: Scott Huang <shili.huang@snapmaker.com>

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -38,7 +38,7 @@
   /**
    * Marlin release version identifier
    */
-  #define SHORT_BUILD_VERSION "SM2-4.7.2"
+  #define SHORT_BUILD_VERSION "SM2-5.1.1"
 
   /**
    * Verbose version identifier which should contain a reference to the location
@@ -51,7 +51,7 @@
    * here we define this default string as the date where the latest release
    * version was tagged.
    */
-  #define STRING_DISTRIBUTION_DATE "2024-0603"
+  #define STRING_DISTRIBUTION_DATE "2024-0407"
 
   /**
    * Required minimum Configuration.h and Configuration_adv.h file versions.
