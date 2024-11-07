2fa071c00a5f6a9af3e319581d4a153858382715
huang-w-y <15768623511@163.com>
2024-11-07 10:47:33 +0800

    Fix: fix the issue of abnormal termination of print jobs.

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -51,7 +51,7 @@
    * here we define this default string as the date where the latest release
    * version was tagged.
    */
-  #define STRING_DISTRIBUTION_DATE "2024-1101"
+  #define STRING_DISTRIBUTION_DATE "2024-1107"
 
   /**
    * Required minimum Configuration.h and Configuration_adv.h file versions.
