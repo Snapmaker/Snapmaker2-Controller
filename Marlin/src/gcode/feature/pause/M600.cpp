314c167b0a1ce331d867b10abd32e00908e4aa9c
huang-w-y <15768623511@163.com>
2024-12-16 11:28:16 +0800

    Improvement: M600

--- Marlin/src/gcode/feature/pause/M600.cpp
+++ Marlin/src/gcode/feature/pause/M600.cpp
@@ -159,13 +159,18 @@ void GcodeSuite::M600() {
 #else
 #include "../../../gcode/gcode.h"
 #include "../snapmaker/src/module/toolhead_3dp.h"
+#include "../../../feature/runout.h"
 void GcodeSuite::M600() {
+  planner.synchronize();
+  if (systemservice.GetCurrentStatus() == SYSTAT_WORK) {
+    RunoutResponseDelayed::modify_runout_distance_mm(0.001);
+  }
+  
   #if EXTRUDERS > 1
     printer1->ResetFilamentState(0, actual_extruder);
   #else
     printer1->ResetFilamentState(0, active_extruder);
   #endif
-  event_filament_runout();
 }
 
 #endif // ADVANCED_PAUSE_FEATURE
